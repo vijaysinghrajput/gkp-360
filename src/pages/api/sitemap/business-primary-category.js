34cf90b0377b3415a21ffed06fba6c370eb5dbe4
Vijay Singh <iamvijaysinghrajput@gmail.com>
2025-01-10 23:21:11 +0530

    ok

--- src/pages/api/sitemap/business-primary-category.js
+++ src/pages/api/sitemap/business-primary-category.js
@@ -1,63 +1,12 @@
 import { ProjectSetting } from "../../../config/ProjectSetting";
 
-const buildSitemapXML = (urls) => {
-  const header = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+export default async function handler(req, res) {
+  const domain = ProjectSetting.API_URL;
 
-  const footer = `</urlset>`;
+  const response = await fetch(`${domain}/Sitemap/business_main_category`);
+  const sitemap = await response.text();
 
-  const body = urls
-    .map(
-      (url) => `
-  <url>
-    <loc>${url}</loc>
-    <changefreq>daily</changefreq>
-    <priority>0.8</priority>
-  </url>`
-    )
-    .join("\n");
-
-  return `${header}${body}${footer}`;
-};
-
-const handler = async (req, res) => {
-  try {
-    const response = await fetch(
-      `${ProjectSetting.API_URL}/Website/getAllBusinessPrimaryCategory`
-    );
-    const data = await response.json();
-
-    if (!data.success) {
-      return res.status(500).send("Failed to fetch main categories.");
-    }
-
-    const urls = data.main_category.map(
-      (category) =>
-        `${ProjectSetting.COMPANY_WEBSITE}/${category.slug}/${category.id}/business-main-category`
-    );
-
-    for (const category of data.main_category) {
-      const subCategoryResponse = await fetch(
-        `${ProjectSetting.API_URL}/Website/getBusinessListByCategory/${category.id}`
-      );
-      const subCategoryData = await subCategoryResponse.json();
-
-      if (subCategoryData.success) {
-        subCategoryData.data.forEach((subCategory) => {
-          urls.push(
-            `${ProjectSetting.COMPANY_WEBSITE}/${category.slug}/${subCategory.slug}`
-          );
-        });
-      }
-    }
-
-    const sitemap = buildSitemapXML(urls);
-    res.setHeader("Content-Type", "application/xml");
-    res.status(200).send(sitemap);
-  } catch (error) {
-    console.error("Error generating sitemap:", error);
-    res.status(500).send("Error generating sitemap.");
-  }
-};
-
-export default handler;
+  res.setHeader("Content-Type", "application/xml");
+  res.write(sitemap);
+  res.end();
+}
